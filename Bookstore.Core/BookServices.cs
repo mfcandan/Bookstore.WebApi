38c318bb38fa3772ab2e1e8fc6f3dc7d0db42b9d
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-03-24 11:02:35 +0300

    Initial commit

---
+++ Bookstore.Core/BookServices.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace Bookstore.Core
+{
+    public class BookServices : IBookServices
+    {
+        private readonly IMongoCollection<Book> _books;
+
+        public BookServices(IDbClient dbClient)
+        {
+            _books = dbClient.GetBooksCollection();
+        }
+        public List<Book> GetBooks() => _books.Find(book => true).ToList();
+ 
+    }
+}
+
+/*https://www.youtube.com/watch?v=MNepwvCcKXA 26:40*/
---
+++ Bookstore.WebApi/Controllers/BooksController.cs
@@ -0,0 +1,28 @@
+﻿using Bookstore.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookServices _bookServices;
+        public BooksController(IBookServices bookServices)
+        {
+            _bookServices = bookServices;
+        }
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            return Ok(_bookServices.GetBooks());
+        }
+
+    }
+}
